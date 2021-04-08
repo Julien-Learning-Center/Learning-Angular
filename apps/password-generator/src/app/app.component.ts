3be5893fb5da7b422685f33e902fea669544ef24
Julien Broyard <broyard.dev@gmail.com>
2021-04-08 19:03:08 +0200

    refactor: rename the component selector

--- apps/password-generator/src/app/app.component.ts
+++ apps/password-generator/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'learning-root',
+  selector: 'root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
